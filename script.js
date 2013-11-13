159b6e38d430c996ea9e1bee64c1bf9fd2e64084
David Rajchenbach-Teller <dteller@mozilla.com>
2013-11-13 13:17:16 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function go() {
+  var frame = document.createElement("iframe");
+  frame.src = "frame.html";
+  frame.id = "frame";
+  document.body.appendChild(frame);
+
+  var button = document.getElementById("button");
+  button.addEventListener("click", function() {
+    var content = frame.contentDocument;
+    var input = content.getElementById("input");
+    input.value = "New value!";
+  });
+}
+
+go();
+
