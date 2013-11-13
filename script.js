c8605fb777b6b696fe536b0b3695d979d9ae526d
David Rajchenbach-Teller <dteller@mozilla.com>
2013-11-13 17:23:13 +0100

    Three frames

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 function go(title) {
-  console.log("Step 1", title);
   var frame = document.createElement("iframe");
   frame.src = "frame.html";
   document.body.appendChild(frame);
 
   frame.addEventListener("load", function() {
-    console.log("Step 2", title);
     var content = frame.contentDocument;
     var label = content.createElement("div");
     label.textContent = title;
     content.body.appendChild(label);
-
-    console.log("Step 3", title);
   });
 }
 
 go("Inserted at startup");
-window.setTimeout(function() { go("Inserted after startup"); }, 100);
-
+window.setTimeout(function() { go("Inserted after 100ms"); }, 100);
+window.setTimeout(function() { go("Inserted after 1000ms"); }, 1000);
